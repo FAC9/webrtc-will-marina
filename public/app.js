f0f4bba39fe257ba30a325b4ff279ac1f9f500db
WilliamSavage <williamsavage@hotmail.com>
2017-01-24 14:15:19 +0000

    initiate startConnection function

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@
     audio: true
   };
 
+  // Setting up local video display
   navigator.mediaDevices.getUserMedia(options).then( function(avStream) {
     var videoId = 1;
     var video = document.getElementById("video-display-"+videoId);
@@ -18,13 +19,31 @@
   // Create an endpoints database
   var endpoints = {};
 
+  // RTC helper functions using ES6
+  const startConnection = () => {
+
+    const pc = new RTCPeerConnection({});
+    // get a local stream, show it in a self-view and add it to be sent
+    navigator.getUserMedia({
+      'audio': true,
+      'video': true,
+    }, (stream) => {
+      selfView.src = URL.createObjectURL(stream);
+      pc.addStream(stream);
+    }, logError)
+
+  }
+
   // Create a listener callback
   var listenerCb = function(from, toEndpoint, method, data) {
     switch(method) {
       case 'INIT':
         toEndpoint.data.status = 'FREE';
-
+        break;
       case 'CALL':
+        if(toEndpoint.data.status === "FREE") {
+          startConnection();
+        }
         //do something
         //if accepting, send message back to caller!
         //THEN CREATE YOUR END OF THE CALL.. PEER CONNECTOR
@@ -43,11 +62,11 @@
       }
       listenerCb("", newUser, "INIT");
     },
-    // to be implemented properly!!!
     send: function(from, to, method, data) {
       endpoints[to].cb(from, endpoints[to], method, data);
     }
   }
+
   // Register all users
   signallingChannel.registerUser("user1", {name: "Marina"}, listenerCb);
   signallingChannel.registerUser("user2", {name: "Will"}, listenerCb);
